6289b6b1a7a080086e43b2ccc6531e96abe3d8e6
Supreet <singh.supreet14@gmail.com>
2022-01-15 16:29:21 +0530

    Lexer Stage 1

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -3,33 +3,43 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include "../include/token.hpp"
+#include "../include/common.hpp"
 
-typedef struct Global_Struct{
+typedef struct Global_Struct {
     std::string cmd; 
     int curr_index; 
     int cmd_length; 
     Global_Struct(std::string i_cmd): cmd(i_cmd), curr_index(0), cmd_length(i_cmd.length()){}
 } GLOBAL_STRUCT; 
 
-typedef enum TYPE {
-    IDENTIFIER, 
-    NUMBER, 
-    STRING, 
-    OPERATOR,
-    L_BRAK, 
-    R_BRAK, 
 
-} Type; 
 
 // char_mapper will have the mappers. They will be initialized when the object for the 
 // lexer is made. 
 //
+//
 class Lexer {
     public:
-    std::unordered_map<char, Type> char_mapper; 
     void iterate_cmd(GLOBAL_STRUCT *); 
+
+    Token *make_token(std::string, Type); 
+    std::vector<Token *> list_of_tokens; 
+
+
+    char get_char(int); 
+    Type get_char_type(int); 
+
     Lexer(); 
     ~Lexer(); 
+
+    void reset(); 
+    private: 
+    friend class Token; 
+    Type current_type, next_type;
+    std::string current_value; 
+    bool flagIden; 
+    std::unordered_map<char, Type> char_mapper; 
 }; 
 
 extern GLOBAL_STRUCT *global_cmd_struct; 
@@ -38,4 +48,16 @@ void cmd_to_struct(std::string);
 void clean_up(); 
     
 
-
+/*
+           if(current_type == SKIPPABLE) {
+               flagIden = false; 
+               continue; 
+           } else if(current_type == IDENTIFIER) {
+              flagIden = true;  
+           } else if(current_type != IDENTIFIER || flagIden) {
+              current_type = IDENTIFIER;  
+           } else if(!flagIden && (current_type != next_type)) {
+              make_token(current_value, current_type);  
+              reset(); 
+           }
+*/
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,6 +1,8 @@
 #include "../include/lexer.hpp"
 #include <iostream>
+#include <ostream>
 #include <string>
+#include <tuple>
 
 GLOBAL_STRUCT *global_cmd_struct; 
 Lexer *lexer_ptr; 
@@ -15,15 +17,78 @@ Lexer::Lexer() {
        this->char_mapper[c] = IDENTIFIER; 
     for(char c='0'; c<='9'; c++)
        this->char_mapper[c] = NUMBER;  
+    char_mapper['\n'] = SKIPPABLE; 
+    char_mapper['\t'] = SKIPPABLE; 
+    char_mapper[' '] = SKIPPABLE; 
+    char_mapper[';'] = DELIMITER; 
+    char_mapper['\\'] = DELIMITER;
+
 }
 
 Lexer::~Lexer() {
-    // I can use this to rest the tokens 
+    this->list_of_tokens.clear(); 
+}
+
+// Tokenizing SKIPPABLES MAKE SURE THOSE ARE SKIPPED; 
+// Add a Switch Case thing here. 
+Token* Lexer::make_token(std::string value, Type type) {
+    Token *t = new Token(value, type); 
+    return t; 
 }
 
 void Lexer::iterate_cmd(GLOBAL_STRUCT *global_struct) {
-    for(auto gs : global_struct->cmd) 
-        std::cout<<gs; 
+    for(int index = global_struct->curr_index; index < global_struct->cmd_length; index++) {
+        if(index == global_struct->cmd_length) {
+            current_type = get_char_type(index); 
+            current_value += get_char(index); 
+            list_of_tokens.push_back(make_token(current_value, current_type)); 
+            reset(); 
+        } else {
+           current_type = get_char_type(index);  
+           next_type = get_char_type(index+1); 
+           if(current_type == SKIPPABLE) {
+               flagIden = false; 
+               continue; 
+           }
+           if(current_type == next_type) {
+               current_value += get_char(index); 
+               if(current_type == IDENTIFIER) 
+                   flagIden = true; 
+           } else {
+               if(current_type == IDENTIFIER) {
+                   if(!flagIden) 
+                       flagIden = true;
+                   current_value += get_char(index); 
+               } 
+
+
+               if(flagIden && current_type == NUMBER) {
+                   current_value += get_char(index); 
+               } else if(flagIden && next_type != NUMBER){
+                   list_of_tokens.push_back(make_token(current_value, current_type)); 
+                   reset(); 
+               }
+
+               if(!flagIden && current_type != next_type) {
+                   current_value += get_char(index); 
+                   list_of_tokens.push_back(make_token(current_value, current_type)); 
+                   reset(); 
+               }
+           }
+        }
+    }
+}
+
+void Lexer::reset() {
+    this->current_value = ""; 
+}
+
+char Lexer::get_char(int index) {
+    return (global_cmd_struct->cmd[index]); 
+}
+
+Type Lexer::get_char_type(int index) {
+    return (char_mapper[global_cmd_struct->cmd[index]]); 
 }
 
 void clean_up() {
