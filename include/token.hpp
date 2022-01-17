0a4d5fd42307addcc98bff7d9657343127e7f40d
Supreet <singh.supreet14@gmail.com>
2022-01-17 17:14:12 +0530

    Tokens to be updated with specific Operators

--- include/token.hpp
+++ include/token.hpp
@@ -11,9 +11,14 @@ class Token {
     std::string value_of_token; 
     Type type_of_token; 
 
+
     // Added this to work with the specificity of the token type. 
 
     public:
     Token(std::string v, Type t): value_of_token(v), type_of_token(t) {}; 
+    // Token(std::string v, Type t,  Overload the constructor to give the values to Operators. 
+    // To specify the Operator values. 
+    void set_operator(); 
+    Type get_type(); 
     void show(); 
 }; 
--- src/token.cpp
+++ src/token.cpp
@@ -6,4 +6,10 @@ void Token::show() {
     std::cout<<this->value_of_token<< " "<<this->type_of_token << std::endl; 
 }
 
+Type Token::get_type() {
+    return this->type_of_token; 
+}
 
+void Token::set_operator() {
+
+}
